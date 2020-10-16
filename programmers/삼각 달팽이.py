b9ad99fb054e288a93f409f579f3305f8c296f3c
SunggookCHOI <52439348+SunggookCHOI@users.noreply.github.com>
2020-10-16 15:48:15 +0900

    20201016 프로그래머스 삼각 달팽이

--- programmers/삼각 달팽이.py
+++ programmers/삼각 달팽이.py
@@ -11,35 +11,25 @@ def solution(n):
         if d == 'down':
             if j+1==n or square[j+1][i] != 0:
                 check += 1
-                if square[j][i+1] != 0:
-                    # 끝
-                    break
-                else :
-                    square[j][i+1] = s
-                    i += 1
+                square[j][i+1] = s
+                i += 1
             else:
                 square[j+1][i] = s
                 j += 1
         elif d == 'right':
             if i+1 == n or square[j][i+1] != 0 :
                 check += 1
-                if square[j-1][i-1] == 1:
-                    break
-                else :
-                    square[j-1][i-1] = s
-                    j-=1
-                    i-=1
+                square[j-1][i-1] = s
+                j-=1
+                i-=1
             else :
                 square[j][i+1] = s
                 i += 1
         else :
             if square[j-1][i-1] != 0:
                 check +=1
-                if square[j+1][i] == 1:
-                    break
-                else :
-                    square[j+1][i] = s
-                    j += 1
+                square[j+1][i] = s
+                j += 1
             else :
                 square[j-1][i-1] = s
                 j -= 1
