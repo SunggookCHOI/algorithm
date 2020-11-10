80ca8327c32bff23a21b6ec4603afcf82f720c66
SunggookCHOI <52439348+SunggookCHOI@users.noreply.github.com>
2020-11-10 16:26:32 +0900

    20201110 프로그래머스 n진수 게임

--- programmers/n진수 게임.py
+++ programmers/n진수 게임.py
@@ -1,3 +1,4 @@
+#https://hellya.tistory.com/101
 from collections import deque
 T = "0123456789ABCDEF"
 
@@ -26,3 +27,44 @@ def solution(n, t, m, p):
                 q.popleft()
         i += 1
     return answer
+
+''' 더 효율적인 코드 추가
+from collections import deque
+
+class Num():
+    def __init__(self, base):
+        self.num = [0]
+        self.T = list("0123456789ABCDEF")
+        self.base = base
+    def add(self):
+        self.addOne(1)
+    def addOne(self,depth):
+        if depth > len(self.num):
+            self.num = [1] + self.num
+            return ;
+        if self.num[-depth] == self.base-1:
+            self.num[-depth] = 0
+            self.addOne(depth+1)
+        else:
+            self.num[-depth] += 1
+    def getNum(self):
+        return [self.T[x] for x in self.num]
+    
+def solution(n, t, m, p):
+    q= deque()
+    answer = ''
+    
+    #말해야할 숫자
+    num = Num(n)
+    while len(answer) < t:
+        
+        for s in num.getNum():
+            q.append(s)
+            
+        while len(q) > m:
+            answer+= q[p-1]
+            for _ in range(m):
+                q.popleft()
+        num.add()
+    return answer
+'''
